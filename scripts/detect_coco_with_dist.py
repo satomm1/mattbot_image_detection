010cb56e14d3a8904f051a9bbe82098045f28648
Matthew Sato <satomm@stanford.edu>
2024-07-09 23:51:14 +0000

    Publish detected objects to a custom ros message

--- scripts/detect_coco_with_dist.py
+++ scripts/detect_coco_with_dist.py
@@ -1,7 +1,8 @@
 import rospy
 from sensor_msgs.msg import Image, CameraInfo
 from vision_msgs.msg import Detection3D, Detection3DArray, BoundingBox3D, ObjectHypothesisWithPose
-from geometry_msgs.msg import PoseWithCovariance
+from geometry_msgs.msg import PoseWithCovariance, Pose
+from mattbot_image_detection.msg import DetectedObject, DetectedObjectArray
 import message_filters
 import tf
 from tf.transformations import euler_from_quaternion
@@ -55,9 +56,9 @@ def image_callback(data):
 
     num_detected = len(results[0].boxes.cls)
 
-    detection_array = Detection3DArray()
+    detection_array = DetectedObjectArray()
     detection_array.header.stamp = rospy.Time.now()
-    detection_array.header.frame_id = 'camera_link'
+    detection_array.header.frame_id = 'map'
 
     for i in range(num_detected):
         class_num = results[0].boxes.cls[i].item()
@@ -106,6 +107,11 @@ def unifiedCallback(rgb_data, depth_data):
 
     num_detected = len(results[0].boxes.cls)
 
+    detection_array = DetectedObjectArray()
+    detection_array.header.stamp = rospy.Time.now()
+    detection_array.header.frame_id = 'map'
+    detection_array.objects = []
+
     if num_detected > 0:
         # Convert depth image to numpy array
         depth = np.frombuffer(depth_data.data, dtype=np.uint16).reshape(depth_data.height, depth_data.width)
@@ -151,47 +157,72 @@ def unifiedCallback(rgb_data, depth_data):
             X = X[indx]
             Y = Y[indx]
 
-            estimated_depth = np.mean(depth_values)/1000  # meters
-            print('Estimated depth of {} is {} meters'.format(class_name, estimated_depth))
+            if len(depth_values) == 0:
+                object_dict = {}
+                object_dict['class_name'] = class_name
+                object_dict['depth'] = 0.0
+                object_dict['x_min'] = 0.0
+                object_dict['y_min'] = 0.0
+                object_dict['x_max'] = 0.0
+                object_dict['y_max'] = 0.0
+                object_dict['x_map'] = 0.0
+                object_dict['y_map'] = 0.0
+            else:
 
-            # Get map to camera_link transform
-            (trans, rot) = tf_listener.lookupTransform("/map", "/camera_link", rospy.Time(0))
-            x_camera = trans[0]
-            y_camera = trans[1]
-            _, _, theta_camera = euler_from_quaternion(rot)
-            theta_camera = theta_camera
-            print("Theta camera: ", theta_camera)
+                estimated_depth = np.mean(depth_values)/1000  # meters
+                print('Estimated depth of {} is {} meters'.format(class_name, estimated_depth))
 
-            x_min = np.min(X)
-            y_min = np.min(Y)
-            x_max = np.max(X)
-            y_max = np.max(Y)
+                # Get map to camera_link transform
+                (trans, rot) = tf_listener.lookupTransform("/map", "/camera_link", rospy.Time(0))
+                x_camera = trans[0]
+                y_camera = trans[1]
+                _, _, theta_camera = euler_from_quaternion(rot)
+                theta_camera = theta_camera
+                print("Theta camera: ", theta_camera)
 
-            x_center = (x_min + x_max) / 2
-            y_center = (y_min + y_max) / 2
+                x_min = np.min(X)
+                y_min = np.min(Y)
+                x_max = np.max(X)
+                y_max = np.max(Y)
 
-            hypot = np.sqrt(x_center**2 + estimated_depth**2)
+                x_center = (x_min + x_max) / 2
+                y_center = (y_min + y_max) / 2
 
-            theta_object = np.arctan2(x_center, estimated_depth)
-            x_map = x_camera + hypot * np.cos(theta_camera - theta_object)
-            y_map = y_camera + hypot * np.sin(theta_camera - theta_object)
+                hypot = np.sqrt(x_center**2 + estimated_depth**2)
 
-            object_dict = {}
-            object_dict['class_name'] = class_name
-            object_dict['depth'] = estimated_depth
-            object_dict['x_min'] = x_min
-            object_dict['y_min'] = y_min
-            object_dict['x_max'] = x_max
-            object_dict['y_max'] = y_max
-            object_dict['x_map'] = x_map
-            object_dict['y_map'] = y_map
-            data_dict[j] = object_dict
-            j += 1
+                theta_object = np.arctan2(x_center, estimated_depth)
+                x_map = x_camera + hypot * np.cos(theta_camera - theta_object)
+                y_map = y_camera + hypot * np.sin(theta_camera - theta_object)
 
-            # Write estimated depth on the image
-            image_with_boxes = cv2.putText(image_with_boxes, '{}: {:.2f} m'.format(class_name, estimated_depth), (x[0]+10, y[0]+50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-            image_with_boxes = cv2.putText(image_with_boxes, '{:.2f}, {:.2f}, {:.2f}, {:.2f}'.format(x_min, y_min, x_max, y_max), (x[0]+10, y[0]+70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-            image_with_boxes = cv2.putText(image_with_boxes, '{:.2f}, {:.2f}'.format(x_map, y_map), (x[0]+10, y[0]+90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+                object_dict = {}
+                object_dict['class_name'] = class_name
+                object_dict['depth'] = estimated_depth
+                object_dict['x_min'] = x_min
+                object_dict['y_min'] = y_min
+                object_dict['x_max'] = x_max
+                object_dict['y_max'] = y_max
+                object_dict['x_map'] = x_map
+                object_dict['y_map'] = y_map
+                data_dict[j] = object_dict
+                j += 1
+
+                # Create DetectedObject message
+                detected_object = DetectedObject()
+                detected_object.class_name = class_name
+                detected_object.probability = 1.0
+                detected_object_pose = Pose()
+                detected_object_pose.position.x = x_map
+                detected_object_pose.position.y = y_map
+                detected_object_pose.position.z = 0
+                detected_object_pose.orientation.w = 1
+                detected_object.pose = detected_object_pose
+                detected_object.width = x_max - x_min
+                detection_array.objects.append(detected_object)
+
+                # Write estimated depth on the image
+                image_with_boxes = cv2.putText(image_with_boxes, '{}: {:.2f} m'.format(class_name, estimated_depth), (x[0]+10, y[0]+50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+                image_with_boxes = cv2.putText(image_with_boxes, '{:.2f}, {:.2f}, {:.2f}, {:.2f}'.format(x_min, y_min, x_max, y_max), (x[0]+10, y[0]+70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+                image_with_boxes = cv2.putText(image_with_boxes, '{:.2f}, {:.2f}'.format(x_map, y_map), (x[0]+10, y[0]+90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
 
     # Create the ROS Image and publish it
     image_msg = Image()
@@ -202,6 +233,10 @@ def unifiedCallback(rgb_data, depth_data):
     image_msg.step = 3 * image_with_boxes.shape[1]
     image_msg.header.stamp = rospy.Time.now()
     publisher.publish(image_msg)
+
+    # Publish the detected objects array if any objects exist
+    if len(detection_array.objects) > 0:
+        detected_object_publisher.publish(detection_array)      
 
     end_time = time.time()
     print('Elapsed time: {}'.format(end_time - start_time))
@@ -219,7 +254,7 @@ if __name__ == '__main__':
 
     # Create the publisher that will show image with bounding boxes
     publisher = rospy.Publisher('/camera/color/image_with_boxes', Image, queue_size=1)
-    # detected_object_publisher = rospy.Publisher('/detected_objects', Detection2DArray, queue_size=10)
+    detected_object_publisher = rospy.Publisher('/detected_objects', DetectedObjectArray, queue_size=10)
 
     depth_readings = []
     depth_timestamps = []
